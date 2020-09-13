3648402b7d8fc1483d46a4e595f6c15bf3d42949
Ronny <mwithiamoki@gmail.com>
2020-09-13 23:46:59 +0300

    add class devconfig

--- config.py
+++ config.py
@@ -22,3 +22,14 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
