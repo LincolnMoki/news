78594953ebf3be79a473ed804da076d705cc777c
Ronny <mwithiamoki@gmail.com>
2020-09-13 15:55:05 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app
+
+#views
+@app.route('/')
+def index():
+
+    '''
+    funtion that returns the index page and its data
+    '''
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
+
