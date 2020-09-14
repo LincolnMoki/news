da2b9023be4a8a059a470b8bfc2bcd6e29636f68
Ronny <mwithiamoki@gmail.com>
2020-09-14 16:36:13 +0300

    add define get sources

--- app/request.py
+++ app/request.py
@@ -15,3 +15,21 @@ def configure_request(app):
     base_url = app.config['SOURCES_BASE_URL']
     articles_url = app.config['EVERYTHING_SOURCE_BASE_URL']
 
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
